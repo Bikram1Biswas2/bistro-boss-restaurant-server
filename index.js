ee76cd5afe7f36db598883b6e34a5015de1ce728
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-01-03 16:44:54 +0530

    create cartCollection and insert single cart data to server

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
     app.get('/menu', async(req,res) => {
         const result = await menuCollection.find().toArray()
@@ -42,6 +43,12 @@ async function run() {
         res.send(result)
     })
 
+    //Carts Collection
+    app.post('/carts', async(req,res)=> {
+      const cartItem = req.body
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
